7684cb4a7795aaf1ce92938ba63011f4cb98ebe1
LaurieWired <123765654+LaurieWired@users.noreply.github.com>
2024-01-21 19:49:45 -0800

    Update UI and have project run in background task

--- src/main/java/com/lauriewired/malimite/ui/ProjectWindow.java
+++ src/main/java/com/lauriewired/malimite/ui/ProjectWindow.java
@@ -1,6 +1,8 @@
 package com.lauriewired.malimite.ui;
 
+import javafx.concurrent.Task;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,6 +12,7 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.Separator;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
@@ -62,33 +65,34 @@ public class ProjectWindow {
         Button settingsButton = new Button("Settings");
         settingsButton.setOnAction(e -> ConfigMenu.showSettingsDialog(stage, stage.getScene(), appConfig));
         Button helpButton = new Button("Help");
-
+    
         HBox menuContainer = new HBox(10);
         menuContainer.setPadding(new Insets(10, 10, 10, 10));
+    
         Separator menuSeparator = new Separator();
         menuSeparator.setPrefWidth(Double.MAX_VALUE);
-
+    
         // SplitPane for file contents and decompiled code
         SplitPane splitPane = new SplitPane();
-
+    
         // Root of the entire tree
         TreeItem<String> treeRoot = new TreeItem<>(file.getName());
-
+    
         // Sub-roots for displaying the resources and classes after file processing
         TreeItem<String> resourcesRoot = new TreeItem<>("Resources");
         TreeItem<String> classesRoot = new TreeItem<>("Classes");
-
+    
         // Temp add item to classes node
         createTreeItem(classesRoot, "TestClass");
-
+    
         TreeView<String> lefthandView = new TreeView<>(treeRoot);
         treeRoot.getChildren().add(classesRoot);
         treeRoot.getChildren().add(resourcesRoot);
-
+    
         // Placeholder for decompiled code (right pane)
         StackPane decompiledCodePane = new StackPane();
         decompiledCodePane.getStyleClass().add("decompiled-code-pane");
-
+    
         CodeArea codeArea = new CodeArea();
         codeArea.setParagraphGraphicFactory(paragraphIndex -> {
             Label lineNum = new Label(Integer.toString(paragraphIndex + 1));
@@ -99,13 +103,13 @@ public class ProjectWindow {
         codeArea.setEditable(false);
         codeArea.getStyleClass().add("code-area");
         System.out.println(codeArea.getStyleClass());
-
+    
         decompiledCodePane.getChildren().add(codeArea);
-
+    
         // Add event listener to the TreeView for item selection
         lefthandView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             System.out.println("Node selected: " + (newValue != null ? newValue.getValue() : "null")); // Debugging log
-        
+    
             if (newValue != null && newValue.isLeaf()) {
                 TreeItem<String> parent = newValue.getParent();
                 while (parent != null) {
@@ -117,24 +121,21 @@ public class ProjectWindow {
                 }
             }
         });
-
+    
         splitPane.getItems().addAll(lefthandView, decompiledCodePane);
         splitPane.setDividerPositions(0.2);
-
-        // Set SplitPane to take as much space as possible
-        VBox.setVgrow(splitPane, Priority.ALWAYS);
-
+    
         Button decompilerButton = new Button("Decompiler");
         Button stringsButton = new Button("Strings");
         Button scriptingButton = new Button("Scripting");
         HBox rightButtons = new HBox(10, scriptingButton, decompilerButton, stringsButton);
         rightButtons.setAlignment(Pos.CENTER_RIGHT);
         menuContainer.getChildren().addAll(settingsButton, helpButton, rightButtons);
-
+    
         TextArea console = new TextArea();
         console.setEditable(false);
         console.getStyleClass().add("console");
-
+    
         // Redirect System.out to the TextArea
         PrintStream ps = new PrintStream(new OutputStream() {
             @Override
@@ -146,22 +147,49 @@ public class ProjectWindow {
         });
         System.setOut(ps);
         System.setErr(ps);
-
+    
+        BorderPane consoleArea = new BorderPane();
+        Label terminalLabel = new Label("TERMINAL");
+        terminalLabel.getStyleClass().add("terminal_label");
+        consoleArea.setTop(terminalLabel);
+        consoleArea.setCenter(console);
+    
         populateLefthandTree(resourcesRoot, file);
+    
+        SplitPane mainSplitPane = new SplitPane();
+        mainSplitPane.setOrientation(Orientation.VERTICAL);
+        mainSplitPane.getItems().addAll(splitPane, consoleArea);
+        mainSplitPane.setDividerPositions(0.8); // You can adjust this value as needed
+    
+        // Ensure the mainSplitPane takes up the majority of the space
+        VBox.setVgrow(mainSplitPane, Priority.ALWAYS);
+    
+        // Add all components to the root VBox
+        root.getChildren().addAll(menuContainer, menuSeparator, mainSplitPane);
 
+        initializeProject(malimiteProject, file);
+    }
+
+    private static void initializeProject(Project malimiteProject, File file) {
         // Now initialize the actual project if we have a valid bundle file (has an Info.plist)
         if (!infoPlistPath.isEmpty()) {
-            System.out.println("Opening Malamite project");
-            malimiteProject.processAppBundle(file.getAbsolutePath(), infoPlistPath);
+            System.out.println("Opening Malimite project");
+
+            // Run in the background so it doesn't block the UI
+            Task<Void> task = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    malimiteProject.processAppBundle(file.getAbsolutePath(), infoPlistPath);
+                    return null;
+                }
+            };
+
+            Thread thread = new Thread(task);
+            thread.setDaemon(true); // Mark the thread as a daemon thread so it doesn't prevent application exit
+            thread.start();
         } else {
             System.out.println("No valid Info.plist found");
         }
-
-        // Set a fixed height for the console or make it grow as needed
-        console.setPrefHeight(150);
-
-        // Add all components to the root VBox
-        root.getChildren().addAll(menuContainer, menuSeparator, splitPane, console);
     }
 
     private static void displaySelectedFileContent(TreeItem<String> newValue, CodeArea codeArea, String currentFilePath) {
@@ -179,7 +207,7 @@ public class ProjectWindow {
                 if (zipEntryPath.endsWith("plist") && PlistUtils.isBinaryPlist(contentBytes)) {
                     System.out.println("Handling binary property list");
                     contentText = PlistUtils.decodeBinaryPropertyList(contentBytes);
-                    //this.fileContentArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JSON);
+                    //this.fileContentArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JSON); //TODO
                 } else {
                     contentText = new String(contentBytes);
                     //this.fileContentArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_XML);
